22ccd08c21c903d5494f12758973464152939878
7PH <b.raymond@protonmail.com>
2018-01-10 15:50:06 +0100

    Updated root component

--- home/vhosts/front-end/app/app.module.ts
+++ home/vhosts/front-end/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CguComponent } from './cgu/cgu.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', component: CguComponent }
+];
+
+
+RouterModule.forRoot([ ]);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CguComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
